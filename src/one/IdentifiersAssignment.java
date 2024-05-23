861672776bd764df00272fbe252fa54ff23ab29d
Dilruba <dilruba.sharmin@LEADSOFT.BIZ>
2024-05-23 15:52:27 +0600

    first commit

---
+++ src/one/IdentifiersAssignment.java
@@ -0,0 +1,18 @@
+package one;
+
+public class IdentifiersAssignment {
+    public static void main(String[] args) {
+        String name = "Karim";
+        int number = 7;
+
+        System.out.println(name);
+        System.out.println(number);
+    }
+    public void PrintResult(){
+
+
+
+    }
+
+
+}
